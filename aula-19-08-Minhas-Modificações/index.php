e2d3eb4d5772de55b40f7ab047dd18ce19135b45
rexoliveira <67833327+rexoliveira@users.noreply.github.com>
2022-08-26 21:42:00 -0300

    Aiciona index para redirecionamento

---
+++ aula-19-08-Minhas-Modificações/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta http-equiv="refresh" content="0; URL='./cliente.html'" />
+    <title>REDIRECIONA PARA CLIENTE</title>
+</head>
+<body>
+</body>
