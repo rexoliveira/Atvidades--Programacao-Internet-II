0db29bd19fdceedebe288b3912199ca780a019fe
rexoliveira <roolivei@yahoo.com.br>
2022-08-31 13:50:19 -0300

    Ajustes

--- aula-19-08-Minhas-Modificações/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-
-<head>
-    <meta http-equiv="refresh" content="0; URL='../aula-19-08-Minhas-Modificações/app/cliente.html'" />
-    <title>REDIRECIONA PARA CLIENTE</title>
-</head>
-<body>
-</body>
---
+++ aulas-19-08--26-08-Minhas-Modificações/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta http-equiv="refresh" content="0; URL='../aulas-19-08--26-08-Minhas-Modificações/app/cliente.html'" />
+    <title>REDIRECIONA PARA CLIENTE</title>
+</head>
+<body>
+</body>
